b1e23be15ef340c939d2873499c50e08cac15591
Dylan Shevlin <Dylanshevlinn@gmail.com>
2023-04-15 23:52:22 +0100

    Changes made to the Occlusion
    (not working properly)

--- Assets/ModularCityAlleyPack/Scripts/OcclusionPortalController.cs
+++ Assets/ModularCityAlleyPack/Scripts/OcclusionPortalController.cs
@@ -4,55 +4,44 @@ using UnityEngine;
 
 public class OcclusionPortalController : MonoBehaviour
 {
-    public GameObject targetPortal;
-    public GameObject[] objectsToHide;
+    // An array of GameObjects that should be culled when not visible through the portal
+    public GameObject[] objectsToOcclude;
 
-    private bool isVisible = true;
-
-    void Start()
-    {
-        // Disable objects that are initially hidden
-        foreach (GameObject obj in objectsToHide)
-        {
-            obj.SetActive(false);
-        }
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        // If the player enters the trigger, hide the objects
-        if (other.gameObject.tag == "Player")
-        {
-            isVisible = false;
-            foreach (GameObject obj in objectsToHide)
-            {
-                obj.SetActive(false);
-            }
-        }
-    }
-
-    void OnTriggerExit(Collider other)
-    {
-        // If the player exits the trigger, show the objects
-        if (other.gameObject.tag == "Player")
-        {
-            isVisible = true;
-            foreach (GameObject obj in objectsToHide)
-            {
-                obj.SetActive(true);
-            }
-        }
-    }
+    // The Camera that is rendering the player's view
+    public Camera playerCamera;
 
     void Update()
     {
-        // If the target portal is visible, show the objects
-        if (targetPortal.GetComponent<OcclusionPortal>().open && !isVisible)
+        // Create a plane representing the portal's orientation and position
+        Plane portalPlane = new Plane(transform.forward, transform.position);
+
+        // Create a plane representing the player's view direction and position
+        Plane playerPlane = new Plane(playerCamera.transform.forward, playerCamera.transform.position);
+
+        // Test if any of the objects to occlude are visible through the portal
+        bool portalVisible = GeometryUtility.TestPlanesAABB(
+            new Plane[] { portalPlane },
+            objectsToOcclude[0].GetComponent<Renderer>().bounds);
+
+        // Test if any of the objects to occlude are visible to the player
+        bool playerVisible = GeometryUtility.TestPlanesAABB(
+            new Plane[] { playerPlane },
+            objectsToOcclude[0].GetComponent<Renderer>().bounds);
+
+        // If the portal and the player can see the objects, make them visible
+        if (portalVisible && playerVisible)
         {
-            isVisible = true;
-            foreach (GameObject obj in objectsToHide)
+            foreach (GameObject obj in objectsToOcclude)
             {
                 obj.SetActive(true);
+            }
+        }
+        // If the portal or the player cannot see the objects, hide them
+        else
+        {
+            foreach (GameObject obj in objectsToOcclude)
+            {
+                obj.SetActive(false);
             }
         }
     }
